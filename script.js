068a119a56044f7d4c71f368c8096d6be5fba893
jmaraya1229 <jessemaraya2013@gmail.com>
2022-01-14 19:41:03 -0800

    edit html and js

---
+++ script.js
@@ -0,0 +1,14 @@
+var today = moment().format("dddd, MMMM Do");
+$("#currentDay").text(today);
+
+var time = moment().format();
+var rowPresent = $()
+var rowPast = 
+var rowFuture = 
+
+// change colors
+function changeColors() {
+    for (current==current) {
+        var presentColor = $('present')
+    }
+}
